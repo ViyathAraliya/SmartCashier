ac548fbfa898f9d656008c42db80b89405129961
viyath araliya <viyatharaliya@gmail.com>
2024-05-09 09:34:14 +0530

    Set up backend Spring Boot application using Spring Initializr

---
+++ POS/pointOfSales/pointOfSales/src/main/java/araliya/pointOfSales/PointOfSalesApplication.java
@@ -0,0 +1,13 @@
+package araliya.pointOfSales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSalesApplication.class, args);
+	}
+
+}
---
+++ POS/pointOfSales/pointOfSales/src/test/java/araliya/pointOfSales/PointOfSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package araliya.pointOfSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
